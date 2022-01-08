db8e8dd03dfde0ba3040800be076dcc618755c83
Ivo <ivo.diogo@gmail.com>
2022-01-08 23:42:46 +0000

    Sat Jan  8 23:42:46 WET 2022

--- teste.c
+++ teste.c
@@ -17,29 +17,70 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int file_size()
+int file_size(void)
+{
+    const char *filename;
+    struct stat sb;
+    int size;
 
+    filename = "teste.txt";
+
+    stat(filename, &sb); // funcao que display file status
+
+    return (sb.st_size);
+
+}
+
+
+//read a file and printing it to a string
+/* int c;
+FILE *file;
+file = fopen("test.txt", "r");
+if (file) {
+    while ((c = getc(file)) != EOF)
+        putchar(c);
+    fclose(file);
+} */
 
 int main()
 {
     int fd;
     char *buf;
+    int size_txt;
+     
 
-    fd = open("The Age demanded.txt", O_RDONLY, 0600);
 
-    printf("\n%ld\n\n", sizeof(fd));
+    size_txt = file_size() + 8;
+    
+    fd = open("teste.txt", O_RDONLY);
+     
 
-    buf = (char *)malloc(sizeof(char *) * sizeof(fd));
+    //printf("\n%ld\n\n", sizeof(fd));
 
-    if (fd == -1)
+    buf = (char *)malloc(sizeof(char) * (file_size() + 8));
+
+    /* if (fd == -1)
     {
         printf("\n\nERROR READING!\n");
         return (0);
-    }
-   int LER = read(fd, &buf, 400);
-
-    write(1, &LER, 400);   
+    } */
+   int LER = read(fd, &buf, size_txt);   
     
+   //The read system call takes three arguments: 
+   //The file descriptor of the file. the buffer where the read data is to be stored and. the number of bytes to be read from the file.
+     
+    write(1, &LER, size_txt);  
+    //write(1, "\n\n", 2); 
+
+    /* write thus takes three arguments:
+
+    The file code (file descriptor or fd).
+    The pointer to a buffer where the data is stored (buf).
+    The number of bytes to write from the buffer (nbytes). */
+
+    //printf("%d\n\n", file_size());
+
+    close(fd);     
 
     return (0);
 }
