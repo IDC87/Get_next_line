93668bb5bf7f2d209f5f2aff574d890f53549cf3
Ivo Cruz <ivo.diogo@gmail.com>
2022-02-07 23:41:31 +0000

    Mon Feb  7 23:41:31 WET 2022

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,15 @@
 /*   By: ivda-cru <ivda-cru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 19:18:57 by ivda-cru          #+#    #+#             */
-/*   Updated: 2022/02/07 21:16:26 by ivda-cru         ###   ########.fr       */
+/*   Updated: 2022/02/07 23:23:32 by ivda-cru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-# define BUFFER_SIZE 10
+
+
+# define BUFFER_SIZE 9
 
 /* Duvidas:
 - porque o static variable passa para a outra funcao como double pointer */
@@ -27,13 +29,31 @@
 char *read_line(char **line, char *buf, int fd)
 {
     int byte_read;
+    char *tmp;
+    char *tmp2;
+    char *final;
     
     byte_read = read(fd, buf, BUFFER_SIZE);
 
-    *line = "esta e uma mensagem de teste";
+    printf("\nfirst buffer: %s\n", buf);
+    
+        tmp = malloc(sizeof(char) * byte_read);
+        tmp = buf;    
+
+    byte_read = read(fd, buf, BUFFER_SIZE);
+
+    printf("\nsecond buffer: %s\n", buf);
+
+        tmp2 = malloc(sizeof(char) * byte_read);
+        tmp2 = buf;
+        *line = ft_strjoin(tmp, tmp2);
     
 
-    printf("\n%s\n", *line);
+    /* printf("\nfirst: %s", tmp);
+    printf("\nsecond: %s\n", buf); */
+
+    printf("\nfinal: %s\n", final);
+    
 
     return *line;
     
@@ -51,11 +71,11 @@ char *get_next_line(int fd)
     if (!buf)
         return (NULL);   
 
-    read_line(&line, buf, fd);
+     read_line(&line, buf, fd);
     
     
 
-    return "teste";
+    return line;
 
 }
 
